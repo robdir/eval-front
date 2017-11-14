d9896f1042d69b80766e4824d54a6caddba9d31c
robdir <rhwickham@gmail.com>
2017-11-14 19:40:49 +0100

    Appbar implemented

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-// ...
+import Navigation from './components/UI/Navigation'
+import Routes from './routes'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import muiTheme from './assets/styles/theme'
 import './App.css'
@@ -18,6 +19,7 @@ class App extends Component {
     return (
       <MuiThemeProvider muiTheme={muiTheme}>
         <div className="App">
+        <Navigation />
         <p> Such empty... </p>
         </div>
       </MuiThemeProvider>
