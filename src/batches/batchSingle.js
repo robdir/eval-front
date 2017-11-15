cd3c99ad90b10d78e00e14b7941e27dbe3aa3cbe
robdir <rhwickham@gmail.com>
2017-11-15 20:26:10 +0100

    styling for single batch

--- src/batches/batchSingle.js
+++ src/batches/batchSingle.js
@@ -1,5 +1,7 @@
 import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
+import './batchSingle.css'
 
 class BatchSingle extends PureComponent {
   static propTypes = {
@@ -31,6 +33,7 @@ class BatchSingle extends PureComponent {
       <p> Start Date: {startsAt} </p>
       <p> End Date: {endsAt} </p>
       </ul>
+      <Link to="/"> View all students </Link>
       </div>
       </article>
     )
