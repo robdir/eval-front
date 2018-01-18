788f2736da6f65611ef15083f103a3dc865b6d87
robdir <rhwickham@gmail.com>
2018-01-18 12:32:42 +0100

    batch editor component

--- src/batches/batchSingle.js
+++ src/batches/batchSingle.js
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { push } from 'react-router-redux'
 import RaisedButton from 'material-ui/RaisedButton'
-import Paper from 'material-ui/Paper'
 import './styles/batchSingle.css'
 
 const style = {
