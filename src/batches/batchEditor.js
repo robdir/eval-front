da2d38ee084135f990b4b73d916d8c19937c212f
robdir <rhwickham@gmail.com>
2018-01-18 12:56:12 +0100

    form build

--- src/batches/batchEditor.js
+++ src/batches/batchEditor.js
@@ -6,6 +6,10 @@ import Title from '../components/UI/Title'
 import TextField from 'material-ui/TextField'
 import RaisedButton from 'material-ui/RaisedButton'
 
+const buttonStyle={
+    float: 'right'
+}
+
 class BatchEditor extends PureComponent {
     static propTypes = {
         CREATE_BATCH: PropTypes.func.isRequired,
@@ -26,7 +30,31 @@ class BatchEditor extends PureComponent {
 
     render() {
         return(
-            <Title content="Add a new batch" />
+            <div className='batch_editor'>
+                <Title content="Add a new batch" />
+                    <form onSubmit={this.submitBatch.bind(this)} ref="form">
+                        <div className='input'>
+                            <TextField ref='batchNum' type='number' placeholder='Batch Number'/>
+                        </div>
+                        <div className='input'>
+                            <p> Start Date: </p>
+                            <TextField ref='startDate' type='date'/>
+                        </div>
+                        <div className='input'>
+                            <p> End Date: </p>
+                            <TextField ref='endDate' type='date' />
+                        </div>
+                    </form>
+                    <RaisedButton
+                        style={buttonStyle}
+                        onClick={ this.submitBatch.bind(this)}
+                        label="Add batch"
+                        primary={true}/>
+            </div>
         )
     }
 }
+
+const mapStateToProps = ({ batches }) => ({ batches })
+
+export default connect(mapStateToProps, { CREATE_BATCH })(BatchEditor)
