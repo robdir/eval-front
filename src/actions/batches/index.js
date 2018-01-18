604e4178287bf9bf64130c912eaf475d64fb5c25
robdir <rhwickham@gmail.com>
2018-01-18 12:43:44 +0100

    btch ed comp

--- src/actions/batches/index.js
+++ src/actions/batches/index.js
@@ -1,6 +1,6 @@
 import grabBatches, { GRABBED_BATCHES } from './grab' // default epxport
 import {grabBatch, GRAB_BATCH } from './grab' // NON-DEFAULT EXPORT
-import CREATE_BATCH from './create'
+import {CREATE_BATCH} from './create'
 
 export {
   grabBatches,
--- src/batches/batchEditor.js
+++ src/batches/batchEditor.js
@@ -1,3 +1,32 @@
 import React, {PureComponent} from 'react'
 import { connect } from 'react-redux'
-import {}
+import { CREATE_BATCH } from '../actions/batches'
+import PropTypes from 'prop-types'
+import Title from '../components/UI/Title'
+import TextField from 'material-ui/TextField'
+import RaisedButton from 'material-ui/RaisedButton'
+
+class BatchEditor extends PureComponent {
+    static propTypes = {
+        CREATE_BATCH: PropTypes.func.isRequired,
+    }
+
+    state = {} // necessary?
+
+    submitBatch(event) {
+        event.preventDefault()
+        const batch = {
+            batchNum: this.refs.batchNum.getValue(),
+            startDate: this.refs.startDate.getValue(),
+            endDate: this.refs.endDate.getValue(),
+        }
+        this.props.CREATE_BATCH(batch)
+        this.refs.form.requestFullscreen()
+    }
+
+    render() {
+        return(
+            <Title content="Add a new batch" />
+        )
+    }
+}
